9a4b27b7a168890d10f75a9ba4c74c322c6791a7
g-marcin <marcin.grzmil@hotmail.com>
2023-05-01 15:05:56 +0200

    added axios client

---
+++ src/common/httpClient/httpClient.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+export const httpClient = axios.create({
+  baseURL: `https://quizapi.io/api/`,
+  timeout: 4000,
+  // headers: { Bearer: 'foobar', Authorization: `Bearer marcin` },
+});
+
